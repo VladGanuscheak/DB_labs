5d2ff74773d13ae7e8f252d5ec4d4e5b3b11d8f0
Vlad Ganusceac <36600228+VladGanuscheak@users.noreply.github.com>
2018-11-27 10:37:49 +0200

    Update Lab5_Task4.sql
    Fixed!

--- Lab5/Scripts/Lab5_Task4.sql
+++ Lab5/Scripts/Lab5_Task4.sql
@@ -1,33 +1,36 @@
 Use universitatea
 Go
 
-DECLARE @NOTA INT = 0, @IND INT = 0, @NAME NVARCHAR(20) = '';
+DECLARE @NOTA INT = 0, @IND INT = 0, @FOUND INT = 0, @NAME NVARCHAR(20) = '', @ID_STUDENT INT = 0;
 
-DECLARE @tmp NVARCHAR(20) = '!';
+DECLARE @tmp INT = -1;
 
 BEGIN TRY  
-    WHILE (@IND < 10)
+    WHILE (@FOUND < 100)
 	BEGIN
-		IF (@tmp = @NAME) 
+
+		SELECT TOP(@IND + 1) @NOTA = st_r.Nota, @NAME = st.Nume_Student + ' ' + st.Prenume_Student, @ID_STUDENT = st.Id_Student
+		FROM (studenti as st INNER JOIN studenti_reusita as st_r
+			ON st.Id_Student = st_r.Id_Student) INNER JOIN discipline as d
+			ON st_r.Id_Disciplina = d.Id_Disciplina
+		WHERE D.Disciplina = 'Baze de date' and st_r.Tip_Evaluare LIKE 'Testul 1';
+		
+		IF (@tmp = @ID_STUDENT)
+		 
 			BEGIN
 			RAISERROR ('Less Rows in the Relation!', -- Message text.  
                11, -- Severity.  
                1 -- State.  
                );  
 			END;
-		SET @tmp = @NAME;
+		SET @tmp = @ID_STUDENT;
 
-		SELECT TOP(@IND + 1) @NOTA = st_r.Nota, @NAME = st.Nume_Student + ' ' + st.Prenume_Student
-		FROM (studenti as st INNER JOIN studenti_reusita as st_r
-			ON st.Id_Student = st_r.Id_Student) INNER JOIN discipline as d
-			ON st_r.Id_Disciplina = d.Id_Disciplina
-		WHERE D.Disciplina = 'Baze de date' and st_r.Tip_Evaluare LIKE 'Testul 1';
-		
 		IF (@NOTA <> 6 and @NOTA <> 8)
 		BEGIN
 			SELECT @NAME;
-			SET @IND = @IND + 1;
+			SET @FOUND = @FOUND + 1;
 		END; 
+		SET @IND = @IND + 1; 
 
 END;  
 END TRY  
